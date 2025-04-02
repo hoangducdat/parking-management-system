c330c323790e6b09bb1b61cf8f29a312829fd5fa
hoangducdat <hoangducdat12@gmail.com>
2025-04-03 01:58:21 +0700

    feat: implement SecurityConfig

---
+++ src/main/java/org/project/backend/configuation/SecurityConfig.java
@@ -0,0 +1,48 @@
+package org.project.backend.configuation;
+
+import org.project.backend.constant.Constants;
+import org.project.backend.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+  public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/api/users/register", "/api/users/login").permitAll()
+            .requestMatchers("/api/users/**").hasRole(Constants.ROLE_ADMIN)
+            .requestMatchers("/api/parking/check-in", "/api/parking/check-out",
+                "/api/parking/find", "/api/parking/slots/available").hasAnyRole(Constants.ROLE_STAFF,Constants.ROLE_ADMIN)
+            .requestMatchers("/api/parking/stats").hasRole(Constants.ROLE_ADMIN)
+            .anyRequest().authenticated()
+        )
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
